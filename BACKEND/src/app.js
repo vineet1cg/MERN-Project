18c702af13e7950af9a04ba4d170b93461be90b1
vineet1cg <vineet.prajapati.cg@gmail.com>
2026-02-25 12:02:36 +0530

    fix: remove duplicate response in root route

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -2,45 +2,44 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 const uploadFile = require('./services/storage.service');
-const postModel = require('./models/post.model'); 
+const postModel = require('./models/post.model');
 const cors = require('cors')
 //middleware
 // required for crud operations
 app.use(express.json());
 app.use(cors());
 // for input fields that require more than text data 
-const upload = multer({ storage:multer.memoryStorage() })
+const upload = multer({ storage: multer.memoryStorage() })
 // basic route
-app.get('/',(req,res)=>{
-    res.send("Hello World");
+app.get('/', (req, res) => {
     res.status(200).json({
-        message:"Backend Running :D"
+        message: "Backend Running :D"
     });
 });
 
 // creating a post
-app.post('/create-post',upload.single("image"),async(req,res)=>{
+app.post('/create-post', upload.single("image"), async (req, res) => {
     console.log(req.body);
     console.log(req.file);
     // user can add image and a caption
     const result = await uploadFile(req.file.buffer);
     const post = await postModel.create({
-        image:result.url,
-        caption:req.body.caption
+        image: result.url,
+        caption: req.body.caption
     });
     return res.status(201).json({
-        message : "Post Created Successfully",
+        message: "Post Created Successfully",
         post
     })
 });
 
 // see all posts 
-app.get('/posts',async(req,res)=>{
-    
+app.get('/posts', async (req, res) => {
+
     // all posts
     const posts = await postModel.find()
     return res.status(200).json({
-        message : "Post Fetched Successfully",
+        message: "Post Fetched Successfully",
         posts
     })
 })
